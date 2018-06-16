e6e4d9ac572141f3cd3df69e9588364cbbc99bdb
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2018-06-16 00:10:04 -0300

    WIP python lib

--- python-lib/go_interop.py
+++ python-lib/go_interop.py
@@ -4,13 +4,10 @@ lib = cdll.LoadLibrary("./libpitaya_cluster.dylib")
 
 #[DllImport("libpitaya_cluster", CallingConvention = CallingConvention.Cdecl)]
 #  public static extern bool Shutdown();
-#
-#[DllImport("libpitaya_cluster", CallingConvention = CallingConvention.Cdecl, EntryPoint= "Init")]
-#  static extern bool InitInternal(SDConfig sdConfig, NatsRPCClientConfig rpcClientConfig, NatsRPCServerConfig rpcServerConfig, Server server);
 
-class sd_config(Structure):
+class SdConfig(Structure):
   _fields_ = [
-    ("endpoints", POINTER(c_char_p)),
+    ("endpoints", c_char_p),
     ("endpoints_len", c_int),
     ("etcd_dial_timeout_sec", c_int),
     ("etcd_prefix", c_char_p),
@@ -18,40 +15,42 @@ class sd_config(Structure):
     ("log_heartbeat", c_bool),
     ("sync_servers_interval_sec", c_int)]
 
-class nats_rpc_client_config(Structure):
+class NatsRpcClientConfig(Structure):
   _fields_ = [
     ("endpoint", c_char_p),
     ("max_connection_retries", c_int),
     ("request_timeout_ms", c_int)]
 
-class nats_rpc_server_config(Structure):
+class NatsRpcServerConfig(Structure):
   _fields_ = [
     ("endpoint", c_char_p),
     ("max_connection_retries", c_int),
     ("messages_buffer_size", c_int),
     ("rpc_handle_worker_num", c_int)]
 
-class server(Structure):
+class Server(Structure):
   _fields_ = [
     ("id", c_char_p),
     ("type", c_char_p),
     ("metadata", c_char_p),
     ("frontend", c_bool)]
 
-def py_to_c_str_array(py_str_array):
-  num_str = len(py_str_array)
-  str_array_type = c_char_p * num_str
-  str_array = str_array_type()
-  for i, param in enumerate(py_str_array):
-    str_array[i] = param
-  return str_array
+class GoString(Structure):
+    _fields_ = [("p", c_char_p), ("n", c_longlong)]
 
+class GetServerRes(Structure):
+  _fields_ = [
+    ("server", POINTER(Server)),
+    ("success", c_bool)]
+
+# Init
 lib.Init.restype = c_bool
-lib.Init.argtypes = [sd_config, nats_rpc_client_config, nats_rpc_server_config, server]
-#
-#[DllImport("libpitaya_cluster", CallingConvention = CallingConvention.Cdecl, EntryPoint= "GetServer")]
-#  static extern IntPtr GetServerInternal(GoString id);
-#
+lib.Init.argtypes = [SdConfig, NatsRpcClientConfig, NatsRpcServerConfig, Server]
+
+#GetServer
+lib.GetServer.restype = POINTER(GetServerRes)
+lib.GetServer.argtypes= [GoString]
+
 #[DllImport("libpitaya_cluster", CallingConvention = CallingConvention.Cdecl, EntryPoint= "GetServersByType")]
 #  static extern IntPtr GetServersByTypeInternal(GoString type);
 #
--- python-lib/pitaya_cluster.py
+++ python-lib/pitaya_cluster.py
@@ -1,12 +1,32 @@
 import go_interop as pc
 import uuid as uuid
 
-res = pc.lib.Init(
-    pc.sd_config(pc.py_to_c_str_array(["127.0.0.1:2379"]), 1, 30, "pitaya/", 30, True, 60),
-    pc.nats_rpc_client_config("127.0.0.1:4222", 10, 5000),
-    pc.nats_rpc_server_config("127.0.0.1:4222", 10, 75, 10),
-    pc.server(uuid.uuid4().hex, "python", "{}", False)
+def init(
+    sd_config:pc.SdConfig,
+    nats_rpc_client_config:pc.NatsRpcClientConfig,
+    nats_rpc_server_config:pc.NatsRpcServerConfig,
+    server:pc.Server):
+    return pc.lib.Init(sd_config, nats_rpc_client_config, nats_rpc_server_config, server)
+
+def get_server(server_id:str):
+    res = pc.lib.GetServer(pc.GoString(server_id.encode('utf-8'), len(server_id)))
+    if res.contents.success == False:
+        raise Exception('failed to get server {}'.format(server_id))
+    return res.contents.server.contents
+
+sv_id = uuid.uuid4().hex
+
+res = init(
+    pc.SdConfig(b'127.0.0.1:2379', 1, 30, b'pitaya/', 30, True, 60),
+    pc.NatsRpcClientConfig(b'nats://127.0.0.1:4222', 10, 5000),
+    pc.NatsRpcServerConfig(b'nats://127.0.0.1:4222', 10, 75, 10),
+    pc.Server(sv_id.encode('utf-8'), b'python', b'{}', False),
 )
 
 if res:
-    print("successfully initialized pitaya!")
+    print('successfully initialized pitaya!')
+
+sv = get_server(sv_id)
+print('got sv!', sv.id)
+
+input('Press enter to close program')
