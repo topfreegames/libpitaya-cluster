0b45ecbb66e72f83acbbe67150117f5d9856c865
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2018-10-25 23:43:20 -0300

    wip - etcd error handling

--- cpp-lib/src/pitaya_cluster.cpp
+++ cpp-lib/src/pitaya_cluster.cpp
@@ -1,15 +1,16 @@
 #include "pitaya_cluster.h"
 #include <nats/nats.h>
-//#include <grpc/grpc.h>
-//#include <grpc++/grpc++.h>
 #include <etcd/Client.hpp>
 
 void Test()
 {
     etcd::Client etcd("http://127.0.0.1:4001");
     pplx::task<etcd::Response> response_task = etcd.get("/test/key1");
-    std::cout << "WILL CALL ETCD" << std::endl;
     // ... do something else
     etcd::Response response = response_task.get();
-    std::cout << response.value.value << std::endl;
+    if (response.is_ok()){
+      std::cout << response.value.value << std::endl;
+    } else {
+      std::cout << "error getting key: " << response.status.etcd_error_message << std::endl;
+    }
 }
