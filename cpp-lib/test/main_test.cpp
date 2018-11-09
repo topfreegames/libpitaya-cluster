f86d75156f48b0fbd5b9fd5f9c5d0874a237d520
Leonardo Hahn <leonardo.hahn@tfgco.com>
2018-11-09 17:40:37 -0200

    Fix location of the integration code

--- cpp-lib/test/main_test.cpp
+++ cpp-lib/test/main_test.cpp
@@ -1,5 +1,23 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
+#include "trompeloeil.hpp"
+
+//=============================================
+// Integration with the mocking library
+//=============================================
+namespace trompeloeil {
+template<>
+void
+reporter<specialized>::send(severity s, const char* file, unsigned long line, const char* msg)
+{
+    auto f = line ? file : "[file/line unavailable]";
+    if (s == severity::fatal) {
+        ADD_FAIL_AT(f, line, msg);
+    } else {
+        ADD_FAIL_CHECK_AT(f, line, msg);
+    }
+}
+} // namespace trompeloeil
 
 int
 main(int argc, char* argv[])
