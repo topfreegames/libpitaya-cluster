e853a8d4cf0005f2c4e31af2796fa05563168d1b
Leonardo Hahn <leonardo.hahn@tfgco.com>
2019-03-13 16:07:04 -0300

    Add more constants

--- cpp-lib/include/pitaya/constants.h
+++ cpp-lib/include/pitaya/constants.h
@@ -12,6 +12,15 @@ static constexpr const char* kCodeTimeout = "PIT-504";
 static constexpr const char* kPeerIdKey = "peer.id";
 static constexpr const char* kPeerServiceKey = "peer.service";
 
+static constexpr const char* kGrpcHostKey = "grpc-host";
+static constexpr const char* kGrpcExternalHostKey = "grpc-external-host";
+
+static constexpr const char* kGrpcPortKey = "grpc-host";
+static constexpr const char* kGrpcExternalPortKey = "grpc-external-host";
+
+static constexpr const char* kRegionKey = "region";
+
+} // namespace constants
 } // namespace pitaya
 
 #endif // PITAYA_CONSTANTS_H
