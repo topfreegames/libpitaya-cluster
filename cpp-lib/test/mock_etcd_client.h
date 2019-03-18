f584fdc8a83fca144fc2442098928a20003bb80a
Leonardo Hahn <leonardo.hahn@tfgco.com>
2019-03-18 18:24:13 -0300

    Change mock to make it possible to control onWatch

--- cpp-lib/test/mock_etcd_client.h
+++ cpp-lib/test/mock_etcd_client.h
@@ -15,10 +15,17 @@ public:
     MOCK_METHOD3(Set, pitaya::SetResponse(const std::string&, const std::string&, int64_t));
     MOCK_METHOD1(Get, pitaya::GetResponse(const std::string&));
     MOCK_METHOD1(List, pitaya::ListResponse(const std::string&));
-    MOCK_METHOD1(Watch, void(std::function<void(pitaya::WatchResponse)>));
 
-    MOCK_METHOD0(LeaseKeepAlive, void());
+    void Watch(std::function<void(pitaya::WatchResponse)> onWatch) override
+    {
+        this->onWatch = std::move(onWatch);
+    }
+
+    MOCK_METHOD2(LeaseKeepAlive,
+                 void(int64_t, std::function<void(pitaya::EtcdLeaseKeepAliveStatus)>));
     MOCK_METHOD0(StopLeaseKeepAlive, void());
+
+    std::function<void(pitaya::WatchResponse)> onWatch;
 };
 
 #endif // PITAYA_MOCK_ETCD_CLIENT_H
