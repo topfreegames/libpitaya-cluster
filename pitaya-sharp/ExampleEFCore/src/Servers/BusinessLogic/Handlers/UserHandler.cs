05ad6382e498c1397ad96cde1660cf379296f153
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2019-04-03 15:31:21 -0300

    example-orm async

--- pitaya-sharp/ExampleEFCore/src/Servers/BusinessLogic/Handlers/UserHandler.cs
+++ pitaya-sharp/ExampleEFCore/src/Servers/BusinessLogic/Handlers/UserHandler.cs
@@ -1,61 +1,60 @@
 using System;
-using System.Linq;
+using System.Threading.Tasks;
 using ExampleORM.Models;
 using Gen.Protos;
-using NPitaya;
+using Microsoft.EntityFrameworkCore;
 using NPitaya.Models;
-using Session = Protos.Session;
 using User = Gen.Protos.User;
 
 namespace ExampleORM.Servers.BusinessLogic.Handlers
 {
-    public class UserHandler: BaseHandler
+  public class UserHandler : BaseHandler
+  {
+    private Task<Models.User> GetUserFromToken(Guid token, ExampleContext ctx)
     {
-        private Models.User GetUserFromToken(Guid token, ExampleContext ctx)
-        {
-            return ctx.Users.Single(u => u.Token == token);
-        }
-        
-        private User ModelsUserToProtosUser(Models.User modelsUser)
-        {
-            return new User
-            {
-                Id = modelsUser.Id.ToString(),
-                Name = modelsUser.Name,
-                Token = modelsUser.Token.ToString()
-            };
-        }
-
-        public User Authenticate(PitayaSession session, AuthenticateArgs args)
-        {
-            using (var context = new ExampleContext())
-            {
-                if (args.Token.Length > 0)
-                {
-                    return ModelsUserToProtosUser(GetUserFromToken(new Guid(args.Token), context));
-                }
-
-                // if token was not sent, create an user and return!
-                var newUser = new Models.User();
-                newUser = context.Users.Add(newUser).Entity;
-                context.SaveChanges();
-                return ModelsUserToProtosUser(newUser);
-            }
-        }
-
-        public Answer ChangeName(PitayaSession session, ChangeNameArgs arg)
-        {
-            using (var context = new ExampleContext())
-            {
-                var userModel = GetUserFromToken(new Guid(arg.Token), context);
-                userModel.Name = arg.Name;
-                context.Update(userModel);
-                context.SaveChanges();
-                return new Answer
-                {
-                    Code = "OK!"
-                };
-            }
-        }
+      return ctx.Users.SingleAsync(u => u.Token == token);
     }
+
+    private User ModelsUserToProtosUser(Models.User modelsUser)
+    {
+      return new User
+      {
+        Id = modelsUser.Id.ToString(),
+        Name = modelsUser.Name,
+        Token = modelsUser.Token.ToString()
+      };
+    }
+
+    public async Task<User> Authenticate(PitayaSession session, AuthenticateArgs args)
+    {
+      using (var context = new ExampleContext())
+      {
+        if (args.Token.Length > 0)
+        {
+          return ModelsUserToProtosUser(await GetUserFromToken(new Guid(args.Token), context));
+        }
+
+        // if token was not sent, create an user and return!
+        var newUser = new Models.User();
+        newUser = context.Users.Add(newUser).Entity;
+        await context.SaveChangesAsync();
+        return ModelsUserToProtosUser(newUser);
+      }
+    }
+
+    public async Task<Answer> ChangeName(PitayaSession session, ChangeNameArgs arg)
+    {
+      using (var context = new ExampleContext())
+      {
+        var userModel = await GetUserFromToken(new Guid(arg.Token), context);
+        userModel.Name = arg.Name;
+        context.Update(userModel);
+        await context.SaveChangesAsync();
+        return new Answer
+        {
+          Code = "OK!"
+        };
+      }
+    }
+  }
 }
