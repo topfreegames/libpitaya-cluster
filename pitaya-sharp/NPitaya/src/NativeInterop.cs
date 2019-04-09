f063854d0114d94673f6aa2ffdd0a0b464f3cd9a
Leonardo Hahn <leonardo.hahn@tfgco.com>
2019-04-09 11:13:56 -0300

    Add NPitaya

---
+++ pitaya-sharp/NPitaya/src/NativeInterop.cs
@@ -0,0 +1,214 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace NPitaya
+{
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct CRpc
+    {
+        public IntPtr reqBufferPtr;
+        public IntPtr tag;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Server
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string id;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string type;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string metadata;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string hostname;
+
+        public int frontend;
+
+        public Server(string id, string type, string metadata, string hostname, bool frontend)
+        {
+            this.id = id;
+            this.type = type;
+            this.metadata = metadata;
+            this.hostname = hostname;
+            this.frontend = Convert.ToInt32(frontend);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Error
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string code;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string msg;
+    }
+
+    public enum NativeLogLevel
+    {
+        Debug = 0,
+        Info = 1,
+        Warn = 2,
+        Error = 3,
+        Critical = 4,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SDConfig
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string endpoints;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string etcdPrefix;
+        public int heartbeatTTLSec;
+        public int logHeartbeat;
+        public int logServerSync;
+        public int logServerDetails;
+        public int syncServersIntervalSec;
+
+        public SDConfig(string endpoints, string etcdPrefix, int heartbeatTTLSec, bool logHeartbeat,
+            bool logServerSync, bool logServerDetails, int syncServersIntervalSec)
+        {
+            this.endpoints = endpoints;
+            this.etcdPrefix = etcdPrefix;
+            this.heartbeatTTLSec = heartbeatTTLSec;
+            this.logHeartbeat = Convert.ToInt32(logHeartbeat);
+            this.logServerSync = Convert.ToInt32(logServerSync);
+            this.logServerDetails = Convert.ToInt32(logServerDetails);
+            this.syncServersIntervalSec = syncServersIntervalSec;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GrpcConfig
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string host;
+        public int port;
+        public int connectionTimeoutSec;
+        public int serverShutdownDeadlineMs;
+        public int serverMaxNumberOfRpcs;
+
+        public GrpcConfig(string host, int port, int connectionTimeoutSec,
+            int serverShutdownDeadlineMs, int serverMaxNumberOfRpcs)
+        {
+            this.host = host;
+            this.port = port;
+            this.connectionTimeoutSec = connectionTimeoutSec;
+            this.serverShutdownDeadlineMs = serverShutdownDeadlineMs;
+            this.serverMaxNumberOfRpcs = serverMaxNumberOfRpcs;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Route
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string svType;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string service;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string method;
+
+        public Route(string svType, string service, string method):this(service, method)
+        {
+            this.svType = svType;
+        }
+
+        public Route(string service, string method)
+        {
+            this.service = service;
+            this.method = method;
+            svType = "";
+        }
+
+        public static Route FromString(string r)
+        {
+            string[] res = r.Split(new[] { "." }, StringSplitOptions.None);
+            if (res.Length == 3)
+            {
+                return new Route(res[0], res[1], res[2]);
+            }
+            if (res.Length == 2)
+            {
+                return new Route(res[0], res[1]);
+            }
+            throw new Exception($"invalid route: {r}");
+        }
+
+        public override string ToString()
+        {
+            if (svType.Length > 0)
+            {
+                return $"{svType}.{service}.{method}";
+            }
+            return $"{service}.{method}";
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MemoryBuffer
+    {
+        public IntPtr data;
+        public int size;
+
+        public byte[] GetData()
+        {
+            byte[] data = new byte[this.size];
+            Marshal.Copy(this.data, data, 0, this.size);
+            return data;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct NatsConfig
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string endpoint;
+        public Int64 connectionTimeoutMs;
+        public int requestTimeoutMs;
+        public int maxConnectionRetries;
+        public int maxPendingMessages;
+
+        public NatsConfig(string endpoint, int connectionTimeoutMs, int requestTimeoutMs, int maxConnectionRetries, int maxPendingMessages)
+        {
+            this.endpoint = endpoint;
+            this.connectionTimeoutMs = connectionTimeoutMs;
+            this.requestTimeoutMs = requestTimeoutMs;
+            this.maxConnectionRetries = maxConnectionRetries;
+            this.maxPendingMessages = maxPendingMessages;
+        }
+    }
+}
+
+class StructWrapper : IDisposable
+{
+    public IntPtr Ptr { get; private set; }
+
+    public StructWrapper(object obj)
+    {
+        Ptr = Marshal.AllocHGlobal(Marshal.SizeOf(obj));
+        Marshal.StructureToPtr(obj, Ptr, false);
+    }
+
+    ~StructWrapper()
+    {
+        if (Ptr != IntPtr.Zero)
+        {
+            Marshal.FreeHGlobal(Ptr);
+            Ptr = IntPtr.Zero;
+        }
+    }
+
+    public void Dispose()
+    {
+        Marshal.FreeHGlobal(Ptr);
+        Ptr = IntPtr.Zero;
+        GC.SuppressFinalize(this);
+    }
+
+    public static implicit operator IntPtr(StructWrapper w)
+    {
+        return w.Ptr;
+    }
+}
---
+++ pitaya-sharp/NPitaya/src/PitayaCluster.Native.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace NPitaya
+{
+    public partial class PitayaCluster
+    {
+        private const string LibName = "pitaya_cluster";
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_InitializeWithNats")]
+        private static extern bool InitializeWithNatsInternal(IntPtr natsCfg, IntPtr sdCfg, IntPtr server, NativeLogLevel logLevel, string logFile);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_InitializeWithGrpc")]
+        private static extern bool InitializeWithGrpcInternal(IntPtr grpcCfg, IntPtr sdCfg, IntPtr server, NativeLogLevel logLevel,  string logFile);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_Terminate")]
+        private static extern void TerminateInternal();
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_GetServerById")]
+        private static extern bool GetServerByIdInternal(string serverId, ref Server retServer);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_FreeServer")]
+        private static extern void FreeServerInternal(IntPtr serverPtr);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_RPC")]
+        private static extern unsafe bool RPCInternal(string serverId, string route, IntPtr data, int dataSize, MemoryBuffer** buffer, ref Error retErr);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_FreeMemoryBuffer")]
+        private static extern unsafe void FreeMemoryBufferInternal(MemoryBuffer *ptr);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_OnSignal")]
+        private static extern void OnSignalInternal(OnSignalFunc ptr);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_FreePitayaError")]
+        private static extern unsafe void FreePitayaErrorInternal(ref Error err);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_WaitForRpc")]
+        private static extern IntPtr tfg_pitc_WaitForRpc();
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_FinishRpcCall")]
+        private static extern void tfg_pitc_FinishRpcCall(IntPtr responseMemoryBufferPtr, IntPtr crpcPtr);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_SendPushToUser")]
+        private static extern unsafe bool PushInternal(string serverId, string serverType, IntPtr pushData, MemoryBuffer** buffer, ref Error retErr);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tfg_pitc_SendKickToUser")]
+        private static extern unsafe bool KickInternal(string serverId, string serverType, IntPtr pushData, MemoryBuffer** buffer, ref Error retErr);
+    }
+}
