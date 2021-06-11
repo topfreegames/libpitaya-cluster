e0f84489eb4ede6ad467b64fd2604c95b02c90ac
Rafael da Fonseca <rafael.fonseca@wildlifestudios.com>
2021-06-11 14:27:11 +0100

    Update nats retry settings

--- cpp-lib/src/pitaya/nats_client.cpp
+++ cpp-lib/src/pitaya/nats_client.cpp
@@ -65,6 +65,7 @@ NatsClientImpl::NatsClientImpl(NatsApiType apiType,
 
     natsOptions_SetTimeout(_opts, config.connectionTimeout.count());
     natsOptions_SetMaxReconnect(_opts, config.maxReconnectionAttempts);
+    natsOptions_SetReconnectWait(_opts, config.reconnectWait);
     natsOptions_SetClosedCB(_opts, ClosedCb, this);
     natsOptions_SetDisconnectedCB(_opts, DisconnectedCb, this);
     natsOptions_SetReconnectedCB(_opts, ReconnectedCb, this);
