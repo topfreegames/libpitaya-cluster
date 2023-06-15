e4f1f848a31f2a3a973e97d27950d2ea7a2616e7
Rafael da Fonseca <rafael.fonseca@wildlifestudios.com>
2023-06-15 11:31:40 +0100

    set nats ping interval to correctly use milliseconds

--- cpp-lib/src/pitaya/nats_client.cpp
+++ cpp-lib/src/pitaya/nats_client.cpp
@@ -75,7 +75,7 @@ NatsClientImpl::NatsClientImpl(NatsApiType apiType,
     natsOptions_SetClosedCB(_opts, ClosedCb, this);
     natsOptions_SetDisconnectedCB(_opts, DisconnectedCb, this);
     natsOptions_SetReconnectedCB(_opts, ReconnectedCb, this);
-    natsOptions_SetPingInterval(_opts, 3);
+    natsOptions_SetPingInterval(_opts, 3000);
     natsOptions_SetMaxPingsOut(_opts, 3);
     if (apiType == NatsApiType::Asynchronous) {
         natsOptions_SetMaxPendingMsgs(_opts, config.maxPendingMsgs);
