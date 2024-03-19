8c15986dacf280f6cbade7b08da78c7eafbb0468
Rafael da Fonseca <rafael.fonseca@wildlifestudios.com>
2024-03-19 21:16:23 +0000

    Bump nats.c from 3.3.0 to 3.8.0

--- cpp-lib/deps/nats.c-3.3.0/doc/html/search/classes_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['natsmsglist_0',['natsMsgList',['../structnats_msg_list.html',1,'']]]
-];
--- cpp-lib/deps/nats.c-3.3.0/doc/html/search/groups_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['options_0',['Options',['../group__opts_group.html',1,'']]]
-];
